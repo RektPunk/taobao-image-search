8df8524a7b82b2e7b5974f9dad6aad293658b9b9
RektPunk <rektpunk@gmail.com>
2023-03-24 01:43:10 +0900

    feat - change run py

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
+import argparse
 from module.naver.store_scrapper import NaverStoreInfoScrapper
 
-naver_store_info_scrapper = NaverStoreInfoScrapper()
-store_info_dict = naver_store_info_scrapper.get_store_infos(
-    orig_query="접이식 바텐더 의자",
-    paging_index_limit=4,
-)
-naver_store_info_scrapper.get_best_products()
-best_products_df = naver_store_info_scrapper.get_best_products_details()
-naver_store_info_scrapper.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", help="Input query")
+    parser.add_argument("--page-limit", default=1, help="Set page limit")
+    args = parser.parse_args()
+    assert args.query is not None, "query is None."
+    naver_store_info_scrapper = NaverStoreInfoScrapper()
+    store_info_dict = naver_store_info_scrapper.get_store_infos(
+        orig_query=args.query,
+        paging_index_limit=args.page_limit,
+    )
+    naver_store_info_scrapper.get_best_products()
+    best_products_df = naver_store_info_scrapper.get_best_products_details()
+    naver_store_info_scrapper.close()
