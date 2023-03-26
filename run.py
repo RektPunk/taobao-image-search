a46af07922199053247297486c7bc144876e3ac9
RektPunk <rektpunk@gmail.com>
2023-03-26 14:38:31 +0900

    feat - add wait int args

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import yaml
+import argparse
 from module.naver.store_scrapper import NaverStoreInfoScrapper
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--wait-int", default=3, help="Set wait limit")
+    args = parser.parse_args()
     with open("input_querys.yaml", "r", encoding="utf-8") as input_querys_yaml:
         input_querys = yaml.load(input_querys_yaml, Loader=yaml.FullLoader)
-
-    naver_store_info_scrapper = NaverStoreInfoScrapper()
+    naver_store_info_scrapper = NaverStoreInfoScrapper(wait_int=int(args.wait_int))
     for input_query in input_querys:
         _query = input_query.get("query")
         _page_limit = input_query.get("page_limit")
