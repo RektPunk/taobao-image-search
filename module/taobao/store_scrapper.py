8e48d395dda48a8e7a4e0c7fd22aa8bcf29fcf3a
suinkang <tei91_@naver.com>
2023-03-28 00:13:51 +0900

    pre-commit done

--- module/taobao/store_scrapper.py
+++ module/taobao/store_scrapper.py
@@ -6,14 +6,10 @@ from PIL import Image
 from io import BytesIO
 import win32clipboard
 import os
-from typing import Dict
-from datetime import datetime
-import requests
-from urllib import parse
+import datetime
 import pandas as pd
-from module.taobao.variables import (
-    TaoBaoInfoVariables
-)
+from module.taobao.variables import TaoBaoInfoVariables
+
 
 # 기능 구현 완료 format 맞추기 필요
 class TaoBaoInfoScrapper:
@@ -21,6 +17,10 @@ class TaoBaoInfoScrapper:
         self.driver = webdriver.Chrome("../driver/chromedriver")
         self.wait_int = wait_int
         self.driver.implicitly_wait(self.wait_int)
+        self.today = datetime.date.today().strftime("%Y%m%d")
+        self.image_folder = "images"
+        self.file_path = "files/" + self.today + "_item.csv"
+        self.image_count = 0
 
     def __call__(self):
         return self.driver
@@ -28,202 +28,67 @@ class TaoBaoInfoScrapper:
     def close(self):
         self.driver.close()
 
-    def _get_page(
+    def _send_to_clipboard(self, clip_type, data):
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(clip_type, data)
+        win32clipboard.CloseClipboard()
+
+    def _copy_to_clipboard(self, image_path: str):
+        image = Image.open(image_path)
+
+        output = BytesIO()
+        image.convert("RGB").save(output, "BMP")
+        data = output.getvalue()[14:]
+        output.close()
+        self._send_to_clipboard(win32clipboard.CF_DIB, data)
+
+    def _write_url_file(self, data, file_path):
+        with open(file_path, mode="a", newline="") as file:
+            df = pd.DataFrame(data)
+            df.to_csv(file, index=False, header=not file.tell())
+
+    def _search_taobao_images(
         self,
-        url: str,
+        image_path: str,
+        file_path: str,
     ):
-        self.driver.get(url)
-        self.driver.implicitly_wait(self.wait_int)
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # WEB URL
+        self.driver.get(TaoBaoInfoVariables.TAOBAO_URL)
 
-    def _get_store_infos_for_each_page(
-        self,
-    ) -> Dict[str, str]:
-        malls = self.driver.find_elements(
-            by=By.XPATH, value=NaverStoreInfoVariables.BASICLIST_MALL_AREA
+        # 파일 업로드를 위한 input 요소 클릭
+        upload_element = self.driver.find_element(
+            by=By.XPATH, value=TaoBaoInfoVariables.SEARCHBAR_INPUT_AREA
         )
-        product_infos = self.driver.find_elements(
-            by=By.XPATH, value=NaverStoreInfoVariables.BASICLIST_INFO_AREA
+        upload_element.click()
+        ActionChains(self.driver).key_down(Keys.CONTROL).send_keys("v").key_up(
+            Keys.CONTROL
+        ).perform()
+
+        # submit button 요소 클릭
+        submit_element = self.driver.find_element(
+            by=By.XPATH, value=TaoBaoInfoVariables.COMPONENT_PREVIEW_BUTTON
         )
-        smart_store_info = []
-        for mall, product_info in zip(malls, product_infos):
-            _mall_grade = mall.find_element(
-                by=By.XPATH, value=NaverStoreInfoVariables.BASICLIST_MALL_GRADE
-            ).text
+        submit_element.click()
 
-            if _mall_grade != "":
-                continue
+        # 새창 핸들로 변경
+        self.image_count += 1
+        self.driver.switch_to.window(self.driver.window_handles[self.image_count])
 
-            _title_link = mall.find_element(
-                by=By.XPATH, value=NaverStoreInfoVariables.BASICLIST_MALL_TITLE
-            ).find_element(by=By.CSS_SELECTOR, value="a")
-
-            _product_link = product_info.find_element(
-                by=By.XPATH, value=NaverStoreInfoVariables.BASICLIST_INFO_TITLE
-            ).find_element(by=By.CSS_SELECTOR, value="a")
-            smart_store_info.append(
-                {
-                    "smart_store_link": _title_link.get_attribute("href"),
-                    "smart_store_title": _title_link.text,
-                    "product_link": _product_link.get_attribute("href"),
-                }
-            )
-        return smart_store_info
-
-    def get_store_infos(
-        self,
-        orig_query: str,
-        paging_index_limit: int,
-    ) -> Dict[str, str]:
-        _store_infos = []
-        for paging_index in range(1, paging_index_limit + 1):
-            _url = _generate_url(orig_query=orig_query, paging_index=paging_index)
-            self._get_page(_url)
-            _store_infos_paging_index = self._get_store_infos_for_each_page()
-            _store_infos = _store_infos + _store_infos_paging_index
-        _deduplicated_store_infos = [
-            dict(_store_info_tuple)
-            for _store_info_tuple in {
-                tuple(_store_info.items()) for _store_info in _store_infos
-            }
-        ]
-        self._store_infos = _deduplicated_store_infos
-        return _deduplicated_store_infos
-
-    def _get_best_products_link(
-        self,
-        _smart_store_link: str,
-    ):
-        self._get_page(_smart_store_link)
-        elements_role_presentation = self.driver.find_elements(
-            by=By.CSS_SELECTOR, value=NaverBestProductVariables.LI_ROLE_PRESENTATION
+        # XPath로 요소 찾기
+        element = self.driver.find_element(
+            by=By.XPATH, value=TaoBaoInfoVariables.FIRST_ITEM
         )
-        product_links = []
-        for element in elements_role_presentation:
-            if "찜하기" not in element.text:
-                pass
-            if "상세정보" in element.text:
-                break
-            product_link = element.find_element(
-                by=By.CSS_SELECTOR,
-                value=NaverBestProductVariables.A_CLASS_LINKANCHOR,
-            ).get_attribute("href")
-            product_links.append(product_link)
-        return product_links
+        url_path = element.get_attribute("href")
 
-    def get_best_products(
-        self,
-    ):
-        _key_cols = [
-            "smart_store_title",
-            "smart_store_link",
-            "best_product_link",
-        ]
-        best_products_with_store_info = []
-        for store_info in self._store_infos:
-            _smart_store_link = store_info["product_link"]
-            try:
-                _best_products = self._get_best_products_link(
-                    _smart_store_link=_smart_store_link,
-                )
-            except:
-                continue
-            if len(_best_products) == 0:
-                continue
-            store_info.update({"best_product_link": _best_products})
-            best_products_with_store_info.append(store_info)
+        data = [{"image_path": image_path, "url_path": url_path}]
+        self._write_url_file(data, file_path)
 
-        best_products_df = pd.DataFrame(best_products_with_store_info).explode(
-            "best_product_link"
-        )
-        best_products_df = (
-            best_products_df[_key_cols].drop_duplicates().reset_index(drop=True)
-        )
-        self.best_products_df = best_products_df
-
-    def get_best_products_details(
-        self,
-    ) -> pd.DataFrame:
-        _now = datetime.now().strftime("%Y-%m-%d:%H:%M:%S")
-        image_folder_path = os.path.join("images", f"{_now}")
-        os.makedirs(image_folder_path, exist_ok=True)
-        best_product_details = []
-        for _row_index, best_product in self.best_products_df.iterrows():
-            _smart_store_title = best_product["smart_store_title"]
-            _smart_store_link = best_product["smart_store_link"]
-            _best_product_link = best_product["best_product_link"]
-            self._get_page(
-                _best_product_link,
-            )
-            category_names = self.driver.find_elements(
-                by=By.CLASS_NAME, value=NaverBestProductDetailVariables.CATEGORY
-            )
-            category_names = "|".join(
-                [category_name.text for category_name in category_names]
-            )
-            product_title = self.driver.find_element(
-                by=By.CLASS_NAME, value=NaverBestProductDetailVariables.PRODUCT_TITLE
-            ).text
-            product_price_element = self.driver.find_element(
-                by=By.CLASS_NAME,
-                value=NaverBestProductDetailVariables.PRODUCT_PRICE_ELEMENT,
-            )
-            product_price = product_price_element.find_element(
-                by=By.CLASS_NAME,
-                value=NaverBestProductDetailVariables.PRODUCT_PRICE,
-            ).text.replace(",", "")
-            product_price = int(product_price)
-            product_shipping_price_elements = self.driver.find_elements(
-                by=By.CLASS_NAME,
-                value=NaverBestProductDetailVariables.PRODUCT_SHIPPING_PRICE_ELEMENT,
-            )
-            if "무료배송" in [
-                product_shipping_price_element.text
-                for product_shipping_price_element in product_shipping_price_elements
-            ]:
-                product_shipping_price = 0
+    def get_store_infos(self):
+        for image in os.listdir(self.image_folder):
+            if image.endswith(("jpg", "jpeg", "png")):
+                image_path = self.image_folder + "/" + image
+                self._copy_to_clipboard(image_path)
+                self._search_taobao_images(image_path, self.file_path)
             else:
-                product_shipping_price_elements = self.driver.find_elements(
-                    by=By.CLASS_NAME,
-                    value=NaverBestProductDetailVariables.PRODUCT_SHIPPING_PRICE,
-                )
-                product_shipping_price = int(
-                    [_.text for _ in product_shipping_price_elements][0].replace(
-                        ",", ""
-                    )
-                )
-
-            product_total_price = product_price + product_shipping_price
-            tag_names = self.driver.find_elements(
-                by=By.CLASS_NAME, value=NaverBestProductDetailVariables.TAG_NAME
-            )
-            tag_names = "|".join([tag_name.text for tag_name in tag_names])
-            image_element = self.driver.find_element(
-                by=By.CLASS_NAME, value=NaverBestProductDetailVariables.IMAGE_ELEMENT
-            )
-            image_url = image_element.get_attribute("src")
-            img_data = requests.get(image_url).content
-            image_path = os.path.join(
-                image_folder_path,
-                f"{_row_index}_{_smart_store_title}-{product_title}.png",
-            )
-            with open(image_path, "wb") as handler:
-                handler.write(img_data)
-
-            best_product_detail = {
-                "smart_store_title": _smart_store_title,
-                "smart_store_link": _smart_store_link,
-                "best_product_link": _best_product_link,
-                "category_names": category_names,
-                "product_title": product_title,
-                "product_price": product_price,
-                "product_total_price": product_total_price,
-                "tag_names": tag_names,
-                "image_path": image_path,
-            }
-            best_product_details.append(best_product_detail)
-
-        best_product_details_df = pd.DataFrame(best_product_details)
-        csv_path = os.path.join("files", f"{_now}.csv")
-        best_product_details_df.to_csv(csv_path, index=False)
-        return best_product_details_df
+                pass
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import argparse
 from module.naver.store_scrapper import NaverStoreInfoScrapper
-
+from module.taobao.store_scrapper import TaoBaoInfoScrapper
 
 if __name__ == "__main__":
+    ## Naver Store Info Scrapper
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", help="Input query")
     parser.add_argument("--page-limit", default=1, help="Set page limit")
@@ -16,3 +17,8 @@ if __name__ == "__main__":
     naver_store_info_scrapper.get_best_products()
     best_products_df = naver_store_info_scrapper.get_best_products_details()
     naver_store_info_scrapper.close()
+
+    ## TaoBao Info Scrapper
+    taobao_info_scrapper = TaoBaoInfoScrapper()
+    taobao_info_scrapper.get_store_infos()
+    taobao_info_scrapper.close()
