3148263e849d602a6816963de905f293d04f5906
RektPunk <rektpunk@gmail.com>
2023-04-04 23:25:42 +0900

    feat - remove taobao run

--- naver_run.py
+++ naver_run.py
@@ -27,8 +27,3 @@ if __name__ == "__main__":
     naver_store_info_scrapper.get_category_name()
     naver_store_info_scrapper.save_files()
     naver_store_info_scrapper.close()
-
-    ## TaoBao Info Scrapper
-    taobao_info_scrapper = TaoBaoInfoScrapper()
-    taobao_info_scrapper.get_store_infos()
-    taobao_info_scrapper.close()
