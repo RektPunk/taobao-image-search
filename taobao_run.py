a38ca5b92bc87b1c93ec6d57a585719315cb86b4
RektPunk <rektpunk@gmail.com>
2023-04-05 00:04:49 +0900

    feat - add wait int arg

--- taobao_run.py
+++ taobao_run.py
@@ -1,8 +1,13 @@
+import argparse
 from module.taobao.store_scrapper import TaoBaoInfoScrapper
 
 if __name__ == "__main__":
     ## TaoBao Info Scrapper
-    taobao_info_scrapper = TaoBaoInfoScrapper()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--wait-int", default=3, help="Set wait limit")
+    args = parser.parse_args()
+
+    taobao_info_scrapper = TaoBaoInfoScrapper(wait_int=int(args.wait_int))
     taobao_info_scrapper.get_product_infos()
     taobao_info_scrapper.save_files()
     taobao_info_scrapper.close()
